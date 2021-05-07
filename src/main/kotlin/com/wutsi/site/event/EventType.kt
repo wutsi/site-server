0b7c83b7137120ab712ee23c6f2b060afa990e85
htchepannou <htchepannou@expedia.com>
2021-05-06 22:22:29 -0400

    remove un-used event

--- src/main/kotlin/com/wutsi/site/event/EventType.kt
+++ src/main/kotlin/com/wutsi/site/event/EventType.kt
@@ -3,7 +3,6 @@ package com.wutsi.site.event
 enum class EventType(
     val urn: String
 ) {
-    INVALID("urn:event:wutsi:site:invalid"),
     CREATED("urn:event:wutsi:site:created"),
     UPDATED("urn:event:wutsi:site:updated")
 }
