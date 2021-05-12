99ff02b5007e6c7a606ba5f976ede0158197355a
htchepannou <htchepannou@expedia.com>
2021-05-12 16:46:09 -0400

    integration with the platform

--- src/main/kotlin/com/wutsi/site/Application.kt
+++ src/main/kotlin/com/wutsi/site/Application.kt
@@ -1,13 +1,13 @@
 package com.wutsi.site
 
+import com.wutsi.platform.EnableWutsiCore
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cache.`annotation`.EnableCaching
-import org.springframework.scheduling.`annotation`.EnableAsync
-import org.springframework.scheduling.`annotation`.EnableScheduling
-import org.springframework.security.config.`annotation`.method.configuration.EnableGlobalMethodSecurity
-import org.springframework.security.config.`annotation`.web.configuration.EnableWebSecurity
-import org.springframework.transaction.`annotation`.EnableTransactionManagement
-import kotlin.String
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
 @EnableAsync
@@ -16,6 +16,7 @@ import kotlin.String
 @EnableScheduling
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWutsiCore
 public class Application
 
 public fun main(vararg args: String) {
